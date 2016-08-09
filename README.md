36ccbf7c915627f70f9d4cfe5968ea5d1f389545
pekastel <pablo.adrian.castillo@gmail.com>
2016-08-09 17:24:35 -0300

    Improved Options
    Support added to open windows with a predefined position and size.

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
 # `hyperterm-initial-windows`
 
-Adds support to open a predefined number of windows on init.
+Adds support to open a predefined number of windows with a predefined position and size.
+
+![](https://raw.githubusercontent.com/wiki/pekastel/hyperterm-initial-windows/hyperterm-initial-windows.gif)
 
 # usage
 
 - Open `~/.hyperterm.js`
 - Add `hyperterm-initial-windows` to the list of `plugins`
 - Change `config.initialWindows` to the desired number of windows to open on init, by default opens just one.
+- Set `config.initialWindowsPosition` to specify the position and size for each of the windows to create on init:
+
+    initialWindows: '3', 
+
+    initialWindowsPosition: [
+      {x: 0, y: 0, width: 800, height: 500},
+      {x: 0, y: 550, width: 900, height: 400},
+      {x: 800, y: 0, width: 700, height: 500}
+    ],
 
 # license
 
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+let initialWindows;
+let initialWindowPositions;
+
+exports.decorateConfig = (config) => {
+
+  initialWindows          = config.initialWindows || 0;
+  initialWindowPositions  = config.initialWindowsPosition || [];
+
+  return config;
+
+}
 
 exports.onApp = (app) => {
 
-  let cfg = app.plugins.getDecoratedConfig();
+  if ( initialWindows > 1 ) {
 
-  if ( cfg.initialWindows > 1 ) {
-
-    for (let i = 1; i < cfg.initialWindows; i++) {
+    for (let i = 1; i < initialWindows; i++) {
 
       app.createWindow(function(win){
 
@@ -13,14 +22,41 @@ exports.onApp = (app) => {
 
         let currentPosition = win.getPosition();
 
-        currentPosition[0] = currentPosition[0] + (34 * i);
-        currentPosition[1] = currentPosition[1] + (34 * i);
+        if ( initialWindowPositions[i] ) {
 
-        win.setPosition(currentPosition[0], currentPosition[1]);
+          currentPosition[0] = initialWindowPositions[i].x;
+          currentPosition[1] = initialWindowPositions[i].y;
+
+        } else {
+
+          currentPosition[0] = currentPosition[0] + (34 * i);
+          currentPosition[1] = currentPosition[1] + (34 * i);
+
+        }
+
+        win.setPosition( currentPosition[0], currentPosition[1] );
+        win.setSize( initialWindowPositions[i].width, initialWindowPositions[i].height );
 
       });
 
     };
+
+  }
+
+}
+
+exports.onWindow = (win) => {
+
+  if ( initialWindowPositions ) {
+
+    // XXX: Yep, identifying the first window using the title sucks
+    if ( win.getTitle() === 'HyperTerm' && initialWindowPositions[0] ) {
+
+      win.setPosition( initialWindowPositions[0].x,  initialWindowPositions[0].y );
+
+      win.setSize( initialWindowPositions[0].width, initialWindowPositions[0].height );
+
+    }
 
   }
 
